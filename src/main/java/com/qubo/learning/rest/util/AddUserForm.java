60d7cf434a2e63f4816ebd17b93e767b2559cfe1
Qubo Song <sqb_007@hotmail.com>
2014-03-05 23:38:24 +0800

    update

--- src/main/java/com/qubo/learning/rest/util/AddUserForm.java
+++ src/main/java/com/qubo/learning/rest/util/AddUserForm.java
@@ -1,7 +1,44 @@
 package com.qubo.learning.rest.util;
 
+import com.qubo.learning.common.model.ROLE;
+
 /**
- * Created by Morrandir on 14-3-5.
+ * Created by Qubo_Song on 3/5/14.
  */
 public class AddUserForm {
+    private String userName, password, confirmPassword;
+    private ROLE userRole;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public ROLE getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(ROLE userRole) {
+        this.userRole = userRole;
+    }
+
 }
